2300c4b42442d6942364021b6bd904e068331ef7
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-03-14 17:26:47 +1000

    added url for dev storage

--- auditory/urls.py
+++ auditory/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import IndexView, AuditoriumView
 
@@ -7,3 +9,10 @@ urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('auditorium/', AuditoriumView.as_view(), name="auditorium"),
 ]
+
+
+if settings.DEBUG == "True":
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
