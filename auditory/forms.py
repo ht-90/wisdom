6c0ffed7527e38db9c21e9e0e730cfd97031d6cf
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-03-22 13:25:48 +1000

    updated widgets for thumbnail and audiofile to fix upload error

--- auditory/forms.py
+++ auditory/forms.py
@@ -13,22 +13,19 @@ widget_description = forms.Textarea(attrs={
     "placeholder": "Description",
     "rows": 4
 })
-widget_thumbnail = forms.FileInput(attrs={
-    "class": "mb-5",
-    "type": "file"
-})
-widget_audiofile = forms.FileInput(attrs={
-    "class": "mb-5"
-})
+widget_thumbnail = forms.ClearableFileInput(attrs={'class': 'mb-5'})
+widget_audiofile = forms.ClearableFileInput(attrs={'class': 'mb-5'})
 
 
 class UploadFileForm(forms.ModelForm):
     """File upload form with custom styling"""
     name = forms.CharField(label="", widget=widget_name)
     description = forms.CharField(label="", widget=widget_description)
-    thumbnail = forms.CharField(label="Thumbnail image file", widget=widget_thumbnail)
-    audiofile = forms.CharField(label="Audio file", widget=widget_audiofile)
 
     class Meta:
         model = Audio
         fields = ("name", "description", "thumbnail", "audiofile")
+        widgets = {
+            "thumbnail": widget_thumbnail,
+            "audiofile": widget_audiofile,
+        }
