edfec2f7b458be09d20fefe8277ddb240386088a
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-03-27 13:53:19 +1000

    added signup and user activation view

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import SignUpForm, activate_user
+
+
+class SignUpView(CreateView):
+    template_name = "registration/signup.html"
+    form_class = SignUpForm
+    successful_url = reverse_lazy("login")
+
+
+class UserActivationView(TemplateView):
+    template_name = "registration/user_activation.html"
+
+    def get(self, request, uidb64, token, *args, **kwargs):
+        # Check user token
+        result = activate_user(uidb64, token)
+        # Pass boolean result to context
+        return super().get(request, result=result, **kwargs)
